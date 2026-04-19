1695566285d8d9533df0bc8aca376caf7a5c5ba7
abhish-sudo <p.abhishek8139@gmail.com>
2026-04-20 00:49:48 +0545

    superuser

--- create_superuser.py
+++ create_superuser.py
@@ -1,10 +1,20 @@
 import os
 import django
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'snookernepal.settings')
 django.setup()
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin')
-    print("Superuser created")
+
+username = 'admin'
+password = 'Admin@1234'
+
+if User.objects.filter(username=username).exists():
+    user = User.objects.get(username=username)
+    user.set_password(password)
+    user.save()
+    print("Password reset successfully")
+else:
+    User.objects.create_superuser(username=username, password=password)
+    print("Superuser created successfully")
